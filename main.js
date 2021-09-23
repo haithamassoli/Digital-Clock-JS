1f192ad1bc2a1dd978c08349f1f40a3fc6f0dc70
haithamassoli <haitham.b.assoli@gmail.com>
2021-09-23 14:30:47 +0300

    Digital-Clock-JS

---
+++ main.js
@@ -0,0 +1,23 @@
+function currentTime() {
+  let date = new Date();
+  let hour = date.getHours();
+  let min = date.getMinutes();
+  let sec = date.getSeconds();
+  hour = updateTime(hour);
+  min = updateTime(min);
+  sec = updateTime(sec);
+  document.getElementById("clock").innerText = hour + " : " + min + " : " + sec;
+  let t = setTimeout(() => {
+    currentTime();
+  }, 1000);
+}
+
+function updateTime(k) {
+  if (k < 10) {
+    return "0" + k;
+  } else {
+    return k;
+  }
+}
+
+currentTime();
